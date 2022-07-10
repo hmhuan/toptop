9300b6ebe61ee02df57675f3d6535d6cc2788174
huan-huynh <huan.huynh@linecorp.com>
2022-07-10 21:32:10 +0900

    setup default layout, sidebar

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,14 @@
 import Header from './Header';
 import SideBar from './SideBar';
+import styles from './DefaultLayout.module.scss';
 
 function DefaultLayout({ children }) {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <Header />
-      <div className="container">
+      <div className={styles.container}>
         <SideBar />
-        <div className="content">{children}</div>
+        <div className={styles.content}>{children}</div>
       </div>
     </div>
   );
